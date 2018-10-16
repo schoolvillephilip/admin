8c4ec0f4d70e0ce1e763f28d6bb7ebdee2db7e65
Sokoya Philip <sokoyaphilip@yahoo.com>
2018-10-16 16:46:31 +0100

    Dashboard overview

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -4,33 +4,22 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Dashboard extends CI_Controller{
 
     public function __construct(){
-        // @todo
-        // Check if the user is already logged in
-        // Also check where the user is coming from
-        // $this->session->set_userdata('referred_from', current_url());
         parent::__construct();
-        $this->load->model('seller_model', 'seller');
-        if( !$this->session->userdata('logged_in') ){
+        $this->load->model('admin_model', 'admin');
+        if (!$this->session->userdata('logged_in')) {
             // Ursher the person to where he is coming from
-            if( !empty($this->session->userdata('referred_from')) ) redirect($this->session->userdata('referred_from'));
+            $from = $this->session->userdata('referred_from');
+            if (!empty($from)) redirect($from);
             redirect('login');
-        }        
+        }
     }
 
     public function index(){
-        $page_data['page_title'] = 'Register to be part of the community';
-        $page_data['pg_name'] = 'register';
-        $page_data['meta_tags'] = array('css/bootstrap.min.css',
-            'css/nifty.min.css','css/nifty-demo-icons.min.css','css/nifty-demo.min.css',
-            'plugins/bootstrap-validator/bootstrapValidator.min.css',
-            'plugins/dropzone/dropzone.min.html', 'plugins/bootstrap-markdown/css/bootstrap-markdown.min.css',
-            'plugins/bootstrap-select/bootstrap-select.min.css','plugins/bootstrap-datepicker/bootstrap-datepicker.min.css'
-        );
-        $page_data['scripts'] = array('js/jquery.min.js','js/bootstrap.min.js', 'js/nifty.min.js', 'js/demo/nifty-demo.min.js',
-            'plugins/dropzone/dropzone.min.js','plugins/dropzone/dropzone.min.js','js/demo/form-file-upload.js',
-            'plugins/bootstrap-wizard/jquery.bootstrap.wizard.min.js', 'plugins/bootstrap-validator/bootstrapValidator.min.js',
-            'js/demo/form-wizard.js','plugins/bootstrap-markdown/js/bootstrap-markdown.js', 'plugins/bootstrap-select/bootstrap-select.min.js',
-            'plugins/bootstrap-datepicker/bootstrap-datepicker.min.js');
-        $this->load->view('dashboard', $page_data);
+        $page_data['page_title'] = 'Sellers Overview';
+        $page_data['pg_name'] = 'sellers';
+        $page_data['sub_name'] = 'sellers_overview';
+        $page_data['profile'] = $this->admin->get_profile_details(base64_decode($this->session->userdata('logged_id')),
+            'first_name,last_name,email,profile_pic');
+        $this->load->view('admin/dashboard', $page_data);
     }
 }
