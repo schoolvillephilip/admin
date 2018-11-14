84ba1424043c5f89b02668b4b96004e669be9c76
Adedoyin Michael Adekoniye <michaeladekoniye@gmail.com>
2018-11-14 11:39:43 +0100

    admin dashboard

--- application/views/admin/dashboard.php
+++ application/views/admin/dashboard.php
@@ -34,13 +34,131 @@
                 </div>
                 <!--Page content-->
                 <!--===================================================-->
-                <div id="page-content">
 
-					    <div class="row">
-                            <div class="panel">
+                <div id="page-content">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <div class="panel panel-bordered-info panel-colorful media middle pad-all">
+                                <div class="media-left">
+                                    <div class="pad-hor">
+                                        <i class="demo-psi-cart-coin icon-3x"></i>
+                                    </div>
+                                </div>
+                                <div class="media-body">
+                                    <p class="text-2x mar-no text-semibold">241</p>
+                                    <p class="mar-no">Orders</p>
+                                    <!--Sparkline pie chart -->
+                                    <div>
+                                        <div class="pad-all">
+                                            <p class="mar-no">
+                                                <span class="pull-right text-bold">34</span> Completed
+                                            </p>
+                                            <p class="mar-no">
+                                                <span class="pull-right text-bold">79</span> Total
+                                            </p>
+                                        </div>
+                                        <div class="pad-all">
+                                            <div class="pad-btm">
+                                                <div class="progress progress-sm">
+                                                    <div style="width: 45%;" class="progress-bar progress-bar-info">
+                                                        <span class="sr-only">45%</span>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="pad-btm">
+                                                <div class="progress progress-sm">
+                                                    <div style="width: 89%;" class="progress-bar progress-bar-info">
+                                                        <span class="sr-only">89%</span>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
-					    
+                        <div class="col-md-4">
+                            <div class="panel panel-bordered-mint panel-colorful media middle pad-all">
+                                <div class="media-left">
+                                    <div class="pad-hor">
+                                        <i class="demo-psi-credit-card-2 icon-3x"></i>
+                                    </div>
+                                </div>
+                                <div class="media-body">
+                                    <p class="text-2x mar-no text-semibold">2841</p>
+                                    <p class="mar-no">Sales</p>
+                                    <!--Sparkline pie chart -->
+                                    <div>
+                                        <div class="pad-all">
+                                            <p class="mar-no">
+                                                <span class="pull-right text-bold">34</span> Completed
+                                            </p>
+                                            <p class="mar-no">
+                                                <span class="pull-right text-bold">79</span> Total
+                                            </p>
+                                        </div>
+                                        <div class="pad-all">
+                                            <div class="pad-btm">
+                                                <div class="progress progress-sm">
+                                                    <div style="width: 45%;" class="progress-bar progress-bar-mint">
+                                                        <span class="sr-only">45%</span>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="pad-btm">
+                                                <div class="progress progress-sm">
+                                                    <div style="width: 89%;" class="progress-bar progress-bar-mint">
+                                                        <span class="sr-only">89%</span>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="panel panel-bordered-purple panel-colorful media middle pad-all">
+                                <div class="media-left">
+                                    <div class="pad-hor">
+                                        <i class="demo-psi-refresh icon-3x"></i>
+                                    </div>
+                                </div>
+                                <div class="media-body">
+                                    <p class="text-2x mar-no text-semibold">1</p>
+                                    <p class="mar-no">Disputes</p>
+                                    <!--Sparkline pie chart -->
+                                    <div>
+                                        <div class="pad-all">
+                                            <p class="mar-no">
+                                                <span class="pull-right text-bold">34</span> Completed
+                                            </p>
+                                            <p class="mar-no">
+                                                <span class="pull-right text-bold">79</span> Total
+                                            </p>
+                                        </div>
+                                        <div class="pad-all">
+                                            <div class="pad-btm">
+                                                <div class="progress progress-sm">
+                                                    <div style="width: 45%;" class="progress-bar progress-bar-purple">
+                                                        <span class="sr-only">45%</span>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="pad-btm">
+                                                <div class="progress progress-sm">
+                                                    <div style="width: 89%;" class="progress-bar progress-bar-purple">
+                                                        <span class="sr-only">89%</span>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
                 </div>
                 <!--===================================================-->
                 <!--End page content-->
