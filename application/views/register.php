664c2fe39e4c634406ad7ce81e5cdf53716f760d
Adedoyin Michael Adekoniye <michaeladekoniye@gmail.com>
2018-11-19 21:02:27 +0100

    admin login

--- application/views/register.php
+++ application/views/register.php
@@ -5,19 +5,34 @@
 <!--You may remove all ID or Class names which contain "demo-", they are only used for demonstration. -->
 
 <body>
-    <div id="container" class="cls-container">
-        
-		
-		<!-- BACKGROUND IMAGE -->
-		<!--===================================================-->
-		<div id="bg-overlay"></div>
-		
-		
-		<!-- REGISTRATION FORM -->
-		<!--===================================================-->
-		<div class="cls-content">
-		    <div class="cls-content-lg panel">
-		        <div class="panel-body">
+<div id="container" class="cls-container" style="background-color: #fff !important;">
+
+
+    <!-- BACKGROUND IMAGE -->
+    <!--===================================================-->
+    <div id="bg-overlay"></div>
+
+    <!-- LOGIN FORM -->
+    <!--===================================================-->
+    <div class="cls-content ">
+        <div class="cls-content-lg panel panel-colorful " style="border: 1px solid #26a69a !important;padding-top:10px;">
+            <div class="panel-title" style="background-color: transparent !important;">
+                <div class="row">
+                    <div class="col-md-4">
+
+                    </div>
+                    <div class="col-md-4">
+                        <a href="<?= base_url(); ?>">
+                            <img src="<?= base_url('assets/img/onitshamarket-logo.png'); ?>" alt="<?= lang('app_name'); ?>"
+                                 class="brand-title img-responsive">
+                        </a>
+                    </div>
+                    <div class="col-md-4">
+
+                    </div>
+                </div>
+            </div>
+            <div class="panel-body">
 		            <div class="mar-ver pad-btm">
 		                <h1 class="h3 text-2x">Create a New Account</h1>
 		                <p class="text text-dark">Come join the Carrito community! Let's set up your account.</p>
