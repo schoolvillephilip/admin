70017828af68ceb8859612102d296a57644a7e31
Adedoyin Michael Adekoniye <michaeladekoniye@gmail.com>
2018-11-20 13:52:42 +0100

    admmmmn

--- application/views/settings/payment.php
+++ application/views/settings/payment.php
@@ -41,6 +41,51 @@
                         <h3 class="panel-title">Payment Methods</h3>
                     </div>
                     <div class="panel-body">
+                        <div class="col-md-4">
+                            <div class="panel panel-bordered-default">
+                                <div class="panel-body">
+                                    <b>Interswitch API</b><br><br>
+                                    <i class="fa fa-times fa-fw"></i> Disabled
+                                </div>
+                                <div class="panel-footer">
+                                    <a href="javascript:;" onclick="window.open(this);return false"><i
+                                                class="fa fa-desktop fa-fw"></i></a>
+                                    <a href="javascript:;" onclick="window.open(this);return false"><i
+                                                class="fa fa-book fa-fw"></i></a>
+                                    <a href="javascript:;"><i class="fa fa-cog fa-fw"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="panel panel-bordered-default">
+                                <div class="panel-body">
+                                    <b>Pay on Delivery</b><br><br>
+                                    <i class="fa fa-check fa-fw"></i> Enabled
+                                </div>
+                                <div class="panel-footer">
+                                    <a href="javascript:;" onclick="window.open(this);return false"><i
+                                                class="fa fa-desktop fa-fw"></i></a>
+                                    <a href="javascript:;" onclick="window.open(this);return false"><i
+                                                class="fa fa-book fa-fw"></i></a>
+                                    <a href="javascript:;"><i class="fa fa-cog fa-fw"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="panel panel-bordered-default">
+                                <div class="panel-body">
+                                    <b>Pay Stack</b><br><br>
+                                    <i class="fa fa-times fa-fw"></i> Disabled
+                                </div>
+                                <div class="panel-footer">
+                                    <a href="javascript:;" onclick="window.open(this);return false"><i
+                                                class="fa fa-desktop fa-fw"></i></a>
+                                    <a href="javascript:;" onclick="window.open(this);return false"><i
+                                                class="fa fa-book fa-fw"></i></a>
+                                    <a href="javascript:;"><i class="fa fa-cog fa-fw"></i></a>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <!--===================================================-->
@@ -77,6 +122,6 @@
     <!--=================================================-->
 </div>
 
-    <?php $this->load->view('templates/scripts'); ?>
+<?php $this->load->view('templates/scripts'); ?>
 </body>
 </html>
