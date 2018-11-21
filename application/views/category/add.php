e1f63942a3e44737159faefb16b9b334ec9f15c0
Adedoyin Michael Adekoniye <michaeladekoniye@gmail.com>
2018-11-21 16:33:35 +0100

    Update add.php

--- application/views/category/add.php
+++ application/views/category/add.php
@@ -219,8 +219,6 @@
         });
     });
 
-
-
     var substringMatcher = function(strs) {
         return function findMatches(q, cb) {
             var matches, substringRegex;
@@ -235,16 +233,6 @@
         };
     };
 
-    var data = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
-        'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii',
-        'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana',
-        'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota',
-        'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
-        'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
-        'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island',
-        'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
-        'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
-    ];
     option = [];
     <?php foreach ($options as $option) :  ?>
     option.push("<?=$option->name;?>");
