60dc204a36ed281374402bc55b9d9d6df3bd88a4
Adedoyin Michael Adekoniye <michaeladekoniye@gmail.com>
2019-01-11 12:31:32 +0100

    user roles

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -1,9 +1,12 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
-class MY_Controller extends CI_Controller {
-	
-	function __construct() {
-		parent::__construct();
-		// Security Checks here
-	}
+class MY_Controller extends CI_Controller
+{
+
+    function __construct()
+    {
+        parent::__construct();
+        // Security Checks here
+        $pages = array('dashboard', 'product', 'orders', 'brands', 'categories', 'sellers', 'account', 'settings', 'states', 'profile_settings', 'help', 'logout');
+    }
 }
