78abd6bf1e03d7ad01c4e4bf93167c0fb29b0157
Adedoyin Michael Adekoniye <michaeladekoniye@gmail.com>
2019-01-12 03:39:21 +0100

    Update report.php

--- application/views/account/report.php
+++ application/views/account/report.php
@@ -1,6 +1,6 @@
 <?php $this->load->view('templates/meta_tags'); ?>
 <style>
-    #sellersaleschart {
+    #sales_chart {
         height: 450px;
         border: 1px solid red;
     }
@@ -34,36 +34,52 @@
                     </div>
                     <div class="panel-body">
                         <div class="row">
-                            <div class="col-md-3">
+                            <div class="col-md-3 text-center">
                                 <div class="row">
-                                    <div class="col-md-12" style="height: 145px; border:1px solid aqua;"></div>
+                                    <div class="col-md-12 panel-bordered-default" style="height: 145px;">
+                                        <h5 style="margin-top:35px;">Total Sales</h5>
+                                        <h2>&#8358; 200,000</h2>
+                                    </div>
                                 </div>
                                 <div class="row">
-                                    <div class="col-md-12"
-                                         style="height: 145px; border:1px solid #ffc16f;margin-top:7.5px;"></div>
+                                    <div class="col-md-12 panel-bordered-default"
+                                         style="height: 145px;margin-top:7.5px;">
+                                        <h5 style="margin-top:35px;">Total Delivery</h5>
+                                        <h2>&#8358; 20,000</h2>
+                                    </div>
                                 </div>
                                 <div class="row">
-                                    <div class="col-md-12"
-                                         style="height: 145px; border:1px solid #00ffbc;margin-top:7.5px;"></div>
+                                    <div class="col-md-12 panel-bordered-default"
+                                         style="height: 145px;margin-top:7.5px;">
+                                        <h5 style="margin-top:35px;">Total Commission</h5>
+                                        <h2>&#8358; 10,000</h2>
+                                    </div>
                                 </div>
                             </div>
                             <div class="col-md-6">
-                                <div id="sellersaleschart"></div>
+                                <div id="sales_chart"></div>
                             </div>
-                            <div class="col-md-3">
+                            <div class="col-md-3 text-center">
                                 <div class="row">
-                                    <div class="col-md-12" style="height: 220px; border:1px solid #ffc16f;"></div>
+                                    <div class="col-md-12 panel-bordered-default" style="height: 220px;">
+                                        <h5 style="margin-top:70px;">Total Order Count</h5>
+                                        <h2>200,000</h2>
+                                    </div>
                                 </div>
                                 <div class="row">
-                                    <div class="col-md-12"
-                                         style="height: 220px; border:1px solid #ff80dd;margin-top:10px;"></div>
+                                    <div class="col-md-12 panel-bordered-default"
+                                         style="height: 220px;margin-top:10px;">
+                                        <h5 style="margin-top:70px;">Avg Order Per Customer</h5>
+                                        <h2>2.5</h2>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                         <div class="row" style="margin-top:10px;">
-                            <div class="col-md-9">
+                            <div class="col-md-12">
                                 <h5>Top Products By Order</h5>
-                                <table id="top_selling_products" class="table table-striped table-bordered" cellspacing="0"
+                                <table id="top_selling_products" class="table table-striped table-bordered"
+                                       cellspacing="0"
                                        width="100%">
                                     <thead>
                                     <tr>
@@ -77,17 +93,13 @@
                                     <tbody>
                                     <tr>
                                         <td>1</td>
-                                        <td><img src="me.jpg" alt="Product Image" /></td>
+                                        <td><img src="me.jpg" alt="Product Image"/></td>
                                         <td>Samsung Galaxy J5</td>
+                                        <td>Phones &amp; Tablets</td>
                                         <td>61</td>
                                     </tr>
                                     </tbody>
                                 </table>
-                            </div>
-                            <div class="col-md-3">
-                                <div class="col-md-12" style="border:1px solid blue;height:225px;">
-
-                                </div>
                             </div>
                         </div>
                     </div>
@@ -104,7 +116,7 @@
 <?php $this->load->view('templates/scripts'); ?>
 </body>
 <script>
-    $(document).ready(function() {
+    $(document).ready(function () {
         $("#top_selling_products").dataTable({
             "responsive": true,
             "language": {
@@ -113,6 +125,33 @@
                     "next": '<i class="demo-psi-arrow-right"></i>'
                 }
             }
+        });
+        Morris.Bar({
+            element: 'sales_chart',
+            data: [
+                { y: 'Jan', a: 80},
+                { y: 'Feb', a: 100},
+                { y: 'Mar', a: 75},
+                { y: 'Apr', a: 20},
+                { y: 'May', a: 50},
+                { y: 'June', a: 75},
+                { y: 'July', a: 15},
+                { y: 'Aug', a: 70},
+                { y: 'Sept', a: 100},
+                { y: 'Oct', a: 50},
+                { y: 'Nov', a: 20},
+                { y: 'Dec', a: 40}
+            ],
+            xkey: 'y',
+            ykeys: 'a',
+            labels: ['Total Order'],
+            gridEnabled: true,
+            gridLineColor: 'rgba(0,0,0,.1)',
+            gridTextColor: '#6b7880',
+            gridTextSize: '11px',
+            barColors: ['#179278'],
+            resize:true,
+            hideHover: 'auto'
         });
     });
 </script>
