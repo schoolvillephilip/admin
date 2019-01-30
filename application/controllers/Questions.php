da91cb6fc00860894c646dfff0874a3acc3d106a
Adedoyin Michael Adekoniye <michaeladekoniye@gmail.com>
2019-01-30 15:59:05 +0100

    questions

--- application/controllers/Questions.php
+++ application/controllers/Questions.php
@@ -14,10 +14,36 @@ class Questions extends MY_Controller{
         $page_data['least_sub'] = '';
         $page_data['profile'] = $this->admin->get_profile_details($this->session->userdata('logged_id'),
             'first_name,last_name,email,profile_pic');
-        //$page_data['questions'] = $this->admin->get_message($this->session->userdata('logged_id'));
+        $page_data['questions'] = $this->admin->get_results('qna', "( status != 'approved')")->result();
         $this->load->view('questions', $page_data);
     }
 
+    function approve_question(){
+        if( $this->input->is_ajax_request() ){
+            $qid = $this->input->post('qid');
 
+            if( $this->admin->approve_question($qid) ){
+                echo json_encode(array('status' => 1));
+            }else{
+                $this->session->set_flashdata('error_msg', 'There was an error performing that action. Contact webmaster');
+                echo json_encode(array('status' => 0));
+                exit;
+            }
+        }
+    }
+
+    function decline_question(){
+        if( $this->input->is_ajax_request() ){
+            $qid = $this->input->post('qid');
+
+            if( $this->admin->action($qid,'delete','qna') ){
+                echo json_encode(array('status' => 1));
+            }else{
+                $this->session->set_flashdata('error_msg', 'There was an error performing that action. Contact webmaster');
+                echo json_encode(array('status' => 0));
+                exit;
+            }
+        }
+    }
 
 }
--- application/views/questions.php
+++ application/views/questions.php
@@ -42,57 +42,35 @@
                                     </tr>
                                     </thead>
                                     <tbody>
-                                    <tr>
-                                        <td><img src="<?=base_url('hey.jpg')?>" alt="Samsung S9+"/></td>
-                                        <td>Samsung S9+</td>
-                                        <td>32gb+3gb Ram, 13mp, (dual Sim) 4g - Black</td>
-                                        <td>Chidi</td>
-                                        <td>Akin Akinrounbi</td>
-                                        <td><i class="fa fa-question-circle-o"></i> &nbsp; Is this the 5.6" version?</td>
-                                        <td>12/02/19</td>
-                                        <td><span class="label-warning label">Not Answered Yet</span></td>
-                                        <td>
-                                            <button class="btn btn-success" title="Approve">
-                                                <i class="fa fa-check"></i>
-                                            </button>
-                                            <button class="btn btn-danger mar-lft" title="Decline">
-                                                <i class="fa fa-times"></i></button>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td><img src="<?=base_url('hey.jpg')?>" alt="iPhone XS Max"/></td>
-                                        <td>iPhone XS Max</td>
-                                        <td>128gb+6gb Ram, 18mp, (nano Sim) 4g - Rose Gold</td>
-                                        <td>X-Stores</td>
-                                        <td>Sholagbade Juwon</td>
-                                        <td><i class="fa fa-question-circle-o"></i> &nbsp; What is the difference between this and the XR version?</td>
-                                        <td>12/02/19</td>
-                                        <td><span class="label-warning label">Not Answered Yet</span></td>
-                                        <td>
-                                            <button class="btn btn-success" title="Approve">
-                                                <i class="fa fa-check"></i>
-                                            </button>
-                                            <button class="btn btn-danger mar-lft" title="Decline">
-                                                <i class="fa fa-times"></i></button>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td><img src="<?=base_url('hey.jpg')?>" alt="Cubot King Kong 3"/></td>
-                                        <td>Cubot King Kong 3</td>
-                                        <td>32gb+3gb Ram, 18mp, (mini Sim) 4g - Grey</td>
-                                        <td>Amitek Bashnet</td>
-                                        <td>Chinyere Uzo</td>
-                                        <td><i class="fa fa-question-circle-o"></i> &nbsp; Does it come with a screen guard?</td>
-                                        <td>12/02/19</td>
-                                        <td><span class="label-warning label">Not Answered Yet</span></td>
-                                        <td>
-                                            <button class="btn btn-success" title="Approve">
-                                                <i class="fa fa-check"></i>
-                                            </button>
-                                            <button class="btn btn-danger mar-lft" title="Decline">
-                                                <i class="fa fa-times"></i></button>
-                                        </td>
-                                    </tr>
+                                    <?php foreach ($questions as $question): ?>
+                                        <?php
+                                        $pid = $question->pid;
+                                        $product = $this->admin->get_question_product($pid);
+                                        $product_img = $this->admin->get_question_product_img($pid);
+                                        ?>
+                                        <tr>
+                                            <td><img src="<?= PRODUCTS_IMAGE_PATH . $product_img->image_name; ?>"
+                                                     alt="<?= $product->product_name; ?>"/></td>
+                                            <td><?= $product->product_name; ?></td>
+                                            <td><?= $product->product_description; ?></td>
+                                            <td><?= ucwords($product->first_name . ' ' . $product->last_name); ?></td>
+                                            <td><?= $question->display_name; ?></td>
+                                            <td><i class="fa fa-question-circle-o"></i>
+                                                &nbsp;<?= $question->question; ?>
+                                            </td>
+                                            <td><?= date('l, F d', strtotime($question->qtimestamp)); ?></td>
+                                            <td><span class="label-warning label">Not Answered Yet</span></td>
+                                            <td>
+                                                <button class="btn btn-success approve_question" title="Approve"
+                                                        data-qid="<?= $question->id; ?>">
+                                                    <i class="fa fa-check"></i>
+                                                </button>
+                                                <button class="btn btn-danger mar-lft decline_question" title="Decline"
+                                                        data-qid="<?= $question->id; ?>">
+                                                    <i class="fa fa-times"></i></button>
+                                            </td>
+                                        </tr>
+                                    <?php endforeach; ?>
                                     </tbody>
                                 </table>
                             </div>
@@ -109,11 +87,67 @@
         <i class="pci-chevron chevron-up"></i>
     </button>
 </div>
+<?php $this->load->view('templates/confirm_modal'); ?>
 <?php $this->load->view('templates/scripts'); ?>
 <script src="<?= base_url('assets/plugins/fooTable/dist/footable.all.min.js'); ?>"></script>
 <script>
-    var fooColExp = $('#all-question-col-exp');
+    let self, action = "", qid;
+    let fooColExp = $('#all-question-col-exp');
     fooColExp.footable().trigger('footable_expand_first_row');
+    $('.approve_question').on('click', function () {
+        self = $(this);
+        action = "approve";
+        qid = self.data('qid');
+        $('#modal_confirm')
+            .find('.modal-header')
+            .removeClass('bg-danger').addClass('bg-primary').end()
+            .find('.modal-header > p')
+            .text("Approve Question?").end()
+            .find('.modal-body')
+            .html('<i class="fa fa-check fa-4x text-primary"></i>').end()
+            .modal('show');
+    });
+    $('.decline_question').on('click', function () {
+        self = $(this);
+        action = "decline";
+        qid = self.data('qid');
+        $('#modal_confirm')
+            .find('.modal-header')
+            .removeClass('bg-primary').addClass('bg-danger').end()
+            .find('.modal-header > p')
+            .text("Decline Question?").end()
+            .find('.modal-body')
+            .html('<i class="fa fa-times fa-4x text-danger"></i>').end()
+            .modal('show');
+    });
+
+    $('#confirm_true').on('click', function (e) {
+        e.preventDefault();
+        let url = "";
+        switch (action) {
+            case 'approve':
+                url = 'questions/approve_question';
+                break;
+            case 'decline':
+                url = 'questions/decline_question';
+                break;
+            default:
+                url = "";
+                break;
+        }
+        $.ajax({
+            url: base_url + url,
+            data: {'qid': qid,},
+            type: "POST",
+            dataType: 'json',
+            success: function (d) {
+                window.location.href = base_url + "questions/";
+            },
+            error: function (d) {
+                window.location.href = base_url + "questions/";
+            }
+        });
+    });
 </script>
 </body>
 </html>
