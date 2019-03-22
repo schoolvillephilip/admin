35bfb1f34f0fa12871eba5bf9bc3f378ce9f6fae
Sokoya Philip <philo4u2c@gmail.com>
2019-03-22 15:55:29 +0100

    SELECT 2

--- application/views/product/edit.php
+++ application/views/product/edit.php
@@ -321,8 +321,9 @@
                                                                                             } ?>
                                                                                                 title="<?= $specification['spec_description']; ?>"
                                                                                                 data-width="100%">
+                                                                                            <option value="">--Select--</option>
                                                                                             <?php foreach ($options as $key => $value) : ?>
-                                                                                                <option value="">--Select--</option>
+
                                                                                                 <option
                                                                                                     value="<?= trim($value); ?>"><?= ucwords(trim($value)); ?></option>
                                                                                             <?php endforeach; ?>
